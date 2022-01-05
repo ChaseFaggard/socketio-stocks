15c1359d98e6e5c7351a4c6887e67737bb208029
Chase Faggard <chasefaggard@Chases-MacBook-Pro.local>
2022-01-05 17:33:07 -0600

    All client files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { async } from '@angular/core/testing';
+import { StockService } from './stock.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(private stockService: StockService) { 
+
+    setInterval(this.getLatency, 1000)
+
+    this.getData()
+
+  }
+
+  getData = async () => {
+     let response: Object = await this.stockService.getData({
+       'request-type': 'list'
+     })
+
+     console.log(response)
+
+     response = await this.stockService.getData({
+      'request-type': 'live',
+      symbols: ['ABC']
+    })
+
+    console.log(response)
+  }
+
+  getResponse = async (requestType: string) => {
+    const response = await this.stockService.getResponseType(requestType)
+    console.log(response)
+  }
+
+  getLatency = async () => {
+    const ping = await this.stockService.getLatency()
+    console.log(`Latency: ${ping}ms`)
+  }
+
 }
---
+++ src/app/stock.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { io, Socket } from 'socket.io-client'
+import { environment } from 'src/environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockService {
+
+  private socket: Socket = io(environment.production ? '' : 'http://localhost:8080')
+
+  constructor() { }
+
+  public asyncEmit = (eventName: string, data?: any): Promise<any> => {
+    return new Promise((resolve, reject) => {
+
+      if(data !== undefined) this.socket.emit(eventName, data)
+      else this.socket.emit(eventName)
+      
+      this.socket.on(eventName, (result: any) => {
+        this.socket.off(eventName)
+        resolve(result)
+      })
+
+      /* If no response after 1 second */
+      setTimeout(() => { reject(new Error('Server responded too slow... it might be down or lagging behind')) }, 1000)
+      
+    })
+  }
+
+  public getLatency = async (): Promise<number> => {
+    const startTime = new Date().getTime()
+    await this.asyncEmit('ping')
+    return new Date().getTime() - startTime
+  }
+
+  public getResponseType = async (requestType: string): Promise<Object> => {
+    const response = await this.asyncEmit('requestType', requestType)
+    console.log('Response: ' + JSON.stringify(response))
+    return response
+  }
+
+  public getData = async (request: {'request-type': string, symbols?: string[], start?: string}): Promise<Object> => {
+    return await this.asyncEmit('getData', request)
+  }
+
+
+
+}
