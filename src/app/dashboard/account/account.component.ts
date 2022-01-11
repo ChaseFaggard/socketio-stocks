58d9aebe53ab249b122538ed86542c1a897109f2
Chase Faggard <chasefaggard@Chases-MacBook-Pro.local>
2022-01-11 13:36:49 -0600

    Worked on dashboard styles

--- src/app/dashboard/account/account.component.ts
+++ src/app/dashboard/account/account.component.ts
@@ -24,4 +24,6 @@ export class AccountComponent implements OnInit {
 
   setTheme = (theme: string): void => { this.themeService.setTheme(theme) }
 
+  checkActive = (theme: string): string => theme === this.themeService.theme ? 'active' : ''
+
 }
